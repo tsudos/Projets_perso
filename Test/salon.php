dea1f04703096395ccd509a19ed8bc7da528db50
tsudos <tsud@sfr.fr>
2020-01-12 19:03:03 +0100

    modif des images

--- Test/salon.php
+++ Test/salon.php
@@ -8,8 +8,13 @@
             photo en mode portrait  alors que les 2 autres sont en mode paysage
             donc avec le css qui demande 100% du vh et du vw cela fait étiré    
             <img src="image/essai.gif" atl="acceuil" title="devanture"/>-->
-            <img src="image/paralax/para1" atl="2" title="2"/>
-            <img src="image/paralax/para4" atl="3" title="3"/>
+            <!--essai
+            <img src="image/paralax/para1" atl="2" title="2"/>-->
+            <img src="image/IMG_20191214_160257.gif" atl="1" title="1"/>
+            <img src="image/FB_IMG_1568757459591.jpg" atl="2" title="2"/>
+            <img src="image/paralax/para6.gif" atl="3" title="3"/>
+            <img src="image/paralax/para4.jpg" atl="4" title="4"/>
+            <img src="image/sky-night-dark-black-1816.jpg" atl="5" title="5"/>
             </section>
 <?php
     include_once("_footer.php");
