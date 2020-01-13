d777d21d108158f1c6d215c2e9891ba10329fcf3
tsudos <tsud@sfr.fr>
2020-01-13 09:08:35 +0100

    essai d'un nouveau visuel 3/4-1/4 mis en commentaire car manque d'info sur le flex à travailler

--- Test/salon.php
+++ Test/salon.php
@@ -10,11 +10,11 @@
             <img src="image/essai.gif" atl="acceuil" title="devanture"/>-->
             <!--essai
             <img src="image/paralax/para1" atl="2" title="2"/>-->
-            <img src="image/IMG_20191214_160257.gif" atl="1" title="1"/>
-            <img src="image/FB_IMG_1568757459591.jpg" atl="2" title="2"/>
-            <img src="image/paralax/para6.gif" atl="3" title="3"/>
-            <img src="image/paralax/para4.jpg" atl="4" title="4"/>
-            <img src="image/sky-night-dark-black-1816.jpg" atl="5" title="5"/>
+            <img src="image/IMG_20191214_160257.gif" atl="1" title="1" class="img1"/>
+            <img src="image/FB_IMG_1568757459591.jpg" atl="2" title="2" class="img2"/>
+            <img src="image/paralax/para6.gif" atl="3" title="3" class="img3"/>
+            <img src="image/paralax/para4.jpg" atl="4" title="4" class="img4"/>
+            <img src="image/sky-night-dark-black-1816.jpg" atl="5" title="5" class="img5"/>
             </section>
 <?php
     include_once("_footer.php");
