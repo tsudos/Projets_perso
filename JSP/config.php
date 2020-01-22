4188ae64d465702d556c00a53b4c3804997e6da6
tsudos <tsud@sfr.fr>
2020-01-22 09:06:52 +0100

    rangement des dossiers

---
+++ JSP/config.php
@@ -0,0 +1,17 @@
+<?php
+//(sur la racine : Il stockera nos identifiants de connexion à la BDD 
+
+$config = array(
+
+    'host' => 'localhost',
+    'port' => 3306,
+    'username' => 'tsud',
+    'password' => 'T02s04u81d@jsp',
+    'database' => 'jsp'
+
+);
+
+
+
+
+?>
