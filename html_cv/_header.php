a53ec0a68dd40b99d1373d07e7dfa696e2f6e359
tsudos <tsud@sfr.fr>
2020-01-24 09:25:52 +0100

    deplacement du JS à la fin du fichier html

--- html_cv/_header.php
+++ html_cv/_header.php
@@ -30,12 +30,7 @@
         /*style dans html prioritaire sur le css dans un fichier style*/
         </style>
 
-        <!--permet d'avoir des icones-->
-        <!--<script src="https://kit.fontawesome.com/a076d05399.js"></script>-->
-        <script src="script/awesome.js"></script>
-        <script src="script/scriptText.js"></script>
-        <!--
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>-->
+
         </head>
 
     <body>
